ace001660b05fe412b604fbbef83655f72d3832f
AnastasiyaPlotnikova <world771@yandex.ru>
2022-11-26 19:31:12 +0500

    Added Zadacha36

---
+++ Zadacha36/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях (с нечетными индексами).
+// [3, 7, 23, 12] -> 19
+
+
+int[] CreateRandomArray(int N, int start, int end)
+{
+    int[] RandomArray = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end + 1);
+    }
+    return RandomArray;
+}
+
+int[] array = CreateRandomArray(10, 0, 50);
+Console.WriteLine($"{string.Join(" ", array)}");
+
+int sum = 0;
+for (int i = 1; i < array.Length; i = i + 2)
+{
+    sum = sum + array[i];
+}
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях: {sum}");
